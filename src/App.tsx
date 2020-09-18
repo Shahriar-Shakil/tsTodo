cc10f3ba1a1f58ce241719d2da550bc1ce21bc2e
mrshahriar <toshahriarshakil@gmail.com>
2020-09-19 04:06:06 +0600

    first todo complete with ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function app() {
-  return <div>Hello world</div>;
+import { AddTodoForm } from "./components/add-todo-form";
+import { TodoList } from "./components/todo-list";
+
+const initialTodos: Todo[] = [
+  {
+    text: "Write app",
+    complete: false,
+  },
+  {
+    text: "Write app",
+    complete: true,
+  },
+];
+export default function App() {
+  const [todos, setTodos] = useState(initialTodos);
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+  const addTodo: AddTodo = (text: string) => {
+    const newTodo = { text, complete: false };
+    setTodos([...todos, newTodo]);
+  };
+  return (
+    <>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </>
+  );
 }
